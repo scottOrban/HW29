3a40ee05f1b3ae6e7dbf7e016549c1484a660ac9
scottOrban <48457601+scottOrban@users.noreply.github.com>
2019-04-19 16:38:00 -0600

    led slider

--- msg_box.py
+++ msg_box.py
@@ -1,11 +1,12 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-'''user code'''
-from PyQt5.QtWidgets import QMessageBox
+# -*- coding: utf-8 -*-
 
-def btn_clicked():
-    print("Button Pressed")
-    QMessageBox.information(MainWindow, 'Welcome', 'PyQt5 + Raspberry PI')
-'''user code'''
+# Form implementation generated from reading ui file 'msg_box.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -14,11 +15,15 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(150, 120, 211, 101))
+        self.pushButton.setGeometry(QtCore.QRect(100, 110, 101, 101))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
+        self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.verticalSlider.setGeometry(QtCore.QRect(350, 90, 22, 160))
+        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
+        self.verticalSlider.setObjectName("verticalSlider")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -30,15 +35,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Message Box"))
-        self.pushButton.setText(_translate("MainWindow", "Click me"))
-        '''user code'''
-        self.pushButton.clicked.connect(btn_clicked)
-        
-import sys
-app = QtWidgets.QApplication(sys.argv)
-MainWindow=QtWidgets.QMainWindow()
-ui=Ui_MainWindow()
-ui.setupUi(MainWindow)
-MainWindow.show()
-sys.exit(app.exec_())
-'''user code'''
+        self.pushButton.setText(_translate("MainWindow", "Toggle LED"))
+
